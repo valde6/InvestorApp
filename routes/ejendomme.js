14cf6f5c4508364e6c7e1b366a8dca048e1457db
OscarChristiansen <osch25ac@student.cbs.dk>
2026-04-28 14:16:59 +0200

    Update ejendomme.js

--- routes/ejendomme.js
+++ routes/ejendomme.js
@@ -24,6 +24,7 @@ router.get('/:id', async (req, res) => {
 
         // Hent bygningsdata fra BBR og tag den første aktive bygning
         const bygninger = await findBygninger(husnummerId);
+        //console.log('Bygningskoder:', bygninger.map(b => b.byg021BygningensAnvendelse)); -> kan bruges til at debugge og se hvilke anvendelseskoder der kommer tilbage i data
       
         //Denne linje anvendes, idet der i bygningsarrayet kan være flere bygninger. Find finder den bygning i bygninger, der har koden for "bolig", så man
         //F.eks. ikke ender med en carport eller et udehus -> Det slår fejl når man kører resten af funktionerne og prøver ejs.
